f4daeb5c5721b52e4dc7000cd1c4a539f1671a67
RonaldoMorales <ronaldo05032002@gmail.com>
2024-12-08 20:44:11 -0300

    feat: added forms and used flowbite for better button intereaction

--- src/app/profile-image/profile-image.component.ts
+++ src/app/profile-image/profile-image.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-image',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './profile-image.component.html',
-  styleUrl: './profile-image.component.css'
+  styleUrls: ['./profile-image.component.css']
 })
 export class ProfileImageComponent {
+  contactForm: FormGroup;
+  isSubmitting = false;
+  showPopup = false; 
+  showContactForm = false; 
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      email: [''],
+      mensaje: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.isSubmitting = true;
+
+      
+      setTimeout(() => {
+        this.isSubmitting = false;
+        this.showPopup = true; 
+        this.contactForm.reset();
+      }, 3000);
+    }
+  }
+
+  closePopup() {
+    this.showPopup = false; 
+  }
 }
