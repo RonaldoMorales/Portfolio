27fef31584976e925a63c52c8ee0f9f51f45bd23
RonaldoMorales <ronaldo05032002@gmail.com>
2024-12-08 04:55:16 -0300

    Feat: added image, personal description and professional role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { ProfileImageComponent } from './profile-image/profile-image.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileImageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
